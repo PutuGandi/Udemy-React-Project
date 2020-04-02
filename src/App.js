43bfbed75888dba798cf8de6f1dd46897ec9e69e
Ubuntu <ubuntu@ip-172-31-25-171.ap-southeast-1.compute.internal>
2020-04-02 14:44:11 +0000

    Done

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          PUTU GANDI MITHA WIJAYA
         </p>
         <a
           className="App-link"
